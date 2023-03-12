c1cc47e9168b0d9b6ff1d485e54480b227166c5f
Furkan Çeker <ceker0901@gmail.com>
2023-03-12 13:05:19 +0300

    vue first app

--- 05-Vue-Bind/app.js
+++ 05-Vue-Bind/app.js
@@ -1,15 +1,38 @@
 const app = Vue.createApp({
     template:`
-        <div :style="style">Deneme Yazısı</div>
+    <h2>Frameworkler</h2>
+    <ul>
+        <li v-for="row in frameworks" @click="select(row)" class="list" :class="{selected:row.selected}">
+        {{row.name}}
+        </li>
+    </ul>
+    <h3>Seçilen Frameworkler</h3>
+    <ul>
+        <li v-for="s in selectedTags">
+        {{s.name}}
+        </li>
+    </ul>
     `,
     data() {
         return {
-            style:'color:red'
+            frameworks:[
+                {name:"Vue",selected:true},
+                {name:"Angular",selected:false},
+                {name:"React",selected:false},
+                {name:"Svelte",selected:false}
+            ]
         }
     },
     methods: {
-
+        select(row){
+            row.selected=!row.selected
+        }
     },
+    computed:{
+        selectedTags(){
+            return selecteds=this.frameworks.filter(i=>i.selected)
+        }
+    }
 })
 
 app.mount('#app')
