02933cd548d7157ca09df0e73f0e17735c97ff32
Furkan Çeker <ceker0901@gmail.com>
2023-03-12 11:54:31 +0300

    template & data

--- 02-Vue-App/app.js
+++ 02-Vue-App/app.js
@@ -1,4 +1,15 @@
 const app = Vue.createApp({
-    template:'<h2>This is First App!</h2>'
+    template:`
+    <h2>Başlık : {{baslik}}</h2>
+    <h3>İçerik : {{icerik}}</h3>
+    <p>Yorum Sayısı : {{yorumSayisi}}</p>
+    `,
+    data() {
+        return {
+            baslik:'Vue Dersi',
+            icerik:'Vue.js Konuları',
+            yorumSayisi:7,
+        }
+    }
 })
 app.mount('#app')
