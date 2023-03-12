3167041888aba40367cc28c69994166c72f1ea13
Furkan Çeker <ceker0901@gmail.com>
2023-03-12 16:24:26 +0300

    use of component

---
+++ 07-Vue-CLI-Install/vue-cli-component/src/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ title }}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title:"Vue 3 Giriş"
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
