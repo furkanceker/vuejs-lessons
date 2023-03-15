1eb7162c0a89ccdd29c1ecd2abd3a3f6a95d9efc
Furkan Çeker <ceker0901@gmail.com>
2023-03-16 00:12:48 +0300

    component computed

--- 10-Advanced-Component/components/src/components/Header.vue
+++ 10-Advanced-Component/components/src/components/Header.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
     <slot></slot>
+    <hr>
+    <input type="text" v-model="name">
+    <input type="text" v-model="surname">
+    <p>{{ nameSurname }}</p>
   </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            name:'',
+            surname:'',
+        }
+    },
+    computed: {
+        nameSurname(){
+            return this.name + ' ' + this.surname
+        }
+    }
 }
 </script>
 
