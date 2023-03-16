d633b138301a4f970b4d295d72ac542c1f0e2598
Furkan Çeker <ceker0901@gmail.com>
2023-03-16 21:05:21 +0300

    setup fetch

--- 12-Composition-API/composition-api/src/components/Content.vue
+++ 12-Composition-API/composition-api/src/components/Content.vue
@@ -3,15 +3,41 @@
         <div v-for="content in contents" :key="content.id">
             <span>{{ content.title }}</span>
         </div>
+        <div v-if="err">
+            {{ err }}
+        </div>
     </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
+/*     
     props: ['contents'],
     setup(props) {
         console.log(props.contents)
-    },
+    }, 
+*/
+    setup(){
+        const contents = ref([])
+        const err = ref(null)
+        const getContents = async () => {
+            try {
+                let data = await fetch('https://jsonplaceholder.typicode.com/posts')
+                contents.value = await data.json()
+
+                if(!data.ok){
+                    throw new Error('Verilere Erişilemedi!')
+                } 
+            } catch (error) {
+                err.value = error.message
+            }
+        }
+
+        getContents()
+
+        return { contents, err}
+    }
 }
 </script>
 
