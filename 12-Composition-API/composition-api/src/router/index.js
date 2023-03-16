9fc5da752e09eb2da1ab039827b5c6ddb638b121
Furkan Çeker <ceker0901@gmail.com>
2023-03-16 20:29:27 +0300

    setup computed

--- 12-Composition-API/composition-api/src/router/index.js
+++ 12-Composition-API/composition-api/src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/days',
+    name: 'days',
+    component: () => import('../views/DaysView.vue')
   }
 ]
 
---
+++ 12-Composition-API/composition-api/src/views/DaysView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input type="text" v-model="search">
+        <div v-for="day in finded" :key="day">
+            <span>{{ day }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+import {ref, computed} from "vue"
+
+export default {
+    setup(){
+        const days = ref(['Pazartesi','Salı','Çarşamba','Perşembe','Cuma','Cumartesi','Pazar'])
+        const search = ref('')
+        const finded = computed(()=>{
+            return days.value.filter((day) => day.includes(search.value))
+        })
+        return {days,search,finded}
+    }
+}
+</script>
+
+<style>
+
+</style>
