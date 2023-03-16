1ea34f2d478e3636f19e20ec0eeb61f2938dc700
Furkan Çeker <ceker0901@gmail.com>
2023-03-16 20:05:31 +0300

    setup

---
+++ 12-Composition-API/composition-api/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p>{{ name }} - {{ job }}</p>
+    <button @click="clicked">Tıkla</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import { onMounted } from 'vue';
+
+export default {
+  name: 'HomeView',
+  components: {},
+  setup() {
+    let name = "Furkan"
+    let job  = "Web Developer"
+    const clicked = () => {
+      console.log("Buton tıklandı")
+    }
+    return {name,job,clicked}
+  },
+}
+</script>
