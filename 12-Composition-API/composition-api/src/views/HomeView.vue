542ba36e2c9c4b1aa45301a2b8e9a5d143883f37
Furkan Çeker <ceker0901@gmail.com>
2023-03-16 20:18:22 +0300

    reactive

--- 12-Composition-API/composition-api/src/views/HomeView.vue
+++ 12-Composition-API/composition-api/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>{{ name }} </p>
+    <p>{{ name }} - {{ job.name }} </p>
     <button @click="clicked">Tıkla</button>
     <input type="text" v-model="name">
   </div>
@@ -10,20 +10,22 @@
 <script>
 // @ is an alias to /src
 
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 export default {
   name: 'HomeView',
   components: {},
   setup() {
     let name = ref("Furkan")
+    let job = reactive({name:"Yazılımcı"})
 
     const clicked = () => {
       name.value = "Deneme Click"
+      job.name = "Değer Değişti"
       console.log(name)
     }
 
-    return {name,clicked}
+    return {name,clicked,job}
   },
 }
 </script>
