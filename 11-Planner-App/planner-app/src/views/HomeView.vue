da0292d4708b95fda67005d8b307043861b5b6f5
Furkan Çeker <ceker0901@gmail.com>
2023-03-16 17:22:24 +0300

    filtered data

--- 11-Planner-App/planner-app/src/views/HomeView.vue
+++ 11-Planner-App/planner-app/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <FilterNav @filterChange="current = $event" :current="current"></FilterNav>
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id ">
+      <div v-for="project in filteredProject" :key="project.id ">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
       </div>
     </div>
@@ -38,6 +38,17 @@ export default {
     handleComplete(id) {
       let p = this.projects.find(project => project.id === id)
       p.complete = !p.complete
+    }
+  },
+  computed: {
+    filteredProject() {
+      if(this.current === 'completed'){
+        return this.projects.filter(project => project.complete)
+      }else if(this.current === 'ongoing'){
+        return this.projects.filter(project => !project.complete)
+      }else {
+        return this.projects
+      }
     }
   }
 }
