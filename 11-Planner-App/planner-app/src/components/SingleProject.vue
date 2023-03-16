96b272646844a98f838a946ab4510bc8579ec4dc
Furkan Çeker <ceker0901@gmail.com>
2023-03-16 14:31:38 +0300

    icons

--- 11-Planner-App/planner-app/src/components/SingleProject.vue
+++ 11-Planner-App/planner-app/src/components/SingleProject.vue
@@ -2,6 +2,11 @@
   <div class="project">
     <div class="actions">
         <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
+        <div class="icons">
+            <span class="material-icons">edit</span>
+            <span class="material-icons">delete</span>
+            <span class="material-icons">done</span>
+        </div>
     </div>
     <div v-if="showDetails" class="details">
         <p>{{ project.details }}</p>
@@ -27,13 +32,24 @@ export default {
     padding: 10px 20px;
     border-radius: 5px;
     box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.5);
-    border-left: 4px solid #e90074;
+    border-left: 4px solid #76dd78;
 }
 h3 {
     cursor: pointer;
 }
 .actions {
-
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.material-icons {
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+}
+.material-icons:hover{
+    color: #777;
 }
 .details {
 
