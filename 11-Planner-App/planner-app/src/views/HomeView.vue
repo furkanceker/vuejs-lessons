1b2618f4a20b6980edb463dae77044594f25c029
Furkan Çeker <ceker0901@gmail.com>
2023-03-16 14:06:57 +0300

    planner app install & create db.json file

---
+++ 11-Planner-App/planner-app/src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id ">
+        <h2>{{ project.title }}</h2>
+        <p>{{ project.details }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted(){
+    fetch('http://localhost:3000/projects')
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message))
+  },
+}
+</script>
