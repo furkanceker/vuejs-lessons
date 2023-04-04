c0576628d8e64434511a785f1c7695f8eeff5aa1
Furkan Çeker <ceker0901@gmail.com>
2023-04-04 19:38:17 +0300

    signup & login form

---
+++ 16-Chat-App/chat-app/src/components/SignupForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" required placeholder="Nickname" v-model="nickname">
+    <input type="email" required placeholder="Email" v-model="email">
+    <input type="password" required placeholder="Password" v-model="password">
+    <button>Sign Up</button>
+  </form>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+    setup(){
+        const nickname = ref('')
+        const email = ref('')
+        const password = ref('')
+        const handleSubmit = () => {
+            console.log(nickname.value, email.value, password.value)
+        }
+        return {nickname, email, password, handleSubmit}
+    }
+}
+</script>
+
+<style>
+
+</style>
