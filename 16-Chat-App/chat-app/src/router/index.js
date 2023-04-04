1365bcedf7856706903a268829be2c7be8e3ae57
Furkan Çeker <ceker0901@gmail.com>
2023-04-04 19:01:55 +0300

    init commit

---
+++ 16-Chat-App/chat-app/src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Welcome from '../views/Welcome.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'welcome',
+    component: Welcome
+  },
+  {
+    path: '/chatroom',
+    name: 'chatroom',
+    component: () => import('../views/Chatroom.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
