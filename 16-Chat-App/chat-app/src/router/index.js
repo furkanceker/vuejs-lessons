a4a4d9e00e6d917504d12e7b2b2a2b2ff9a2069c
Furkan Çeker <ceker0901@gmail.com>
2023-04-04 23:38:48 +0300

    route guards

--- 16-Chat-App/chat-app/src/router/index.js
+++ 16-Chat-App/chat-app/src/router/index.js
@@ -1,5 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome.vue'
+import {projectAuth} from '../firebase/config'
+
+const requireAuth = (to,from,next) => {
+  let user = projectAuth.currentUser
+  console.log('current user in auth guard', user)
+  if(!user){
+    next({name:"welcome"})
+  } else {
+    next()
+  }
+}
 
 const routes = [
   {
@@ -10,7 +21,8 @@ const routes = [
   {
     path: '/chatroom',
     name: 'chatroom',
-    component: () => import('../views/Chatroom.vue')
+    component: () => import('../views/Chatroom.vue'),
+    beforeEnter: requireAuth
   }
 ]
 
