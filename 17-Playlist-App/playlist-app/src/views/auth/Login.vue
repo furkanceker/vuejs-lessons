d31e60b2708fc5d2438c3dcb561ce982374a583e
Furkan Çeker <ceker0901@gmail.com>
2023-04-05 15:59:40 +0300

    login view

---
+++ 17-Playlist-App/playlist-app/src/views/auth/Login.vue
@@ -0,0 +1,34 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <h3 class="text-center">Login</h3>
+        <input type="email" placeholder="Email" v-model="email">
+        <input type="password" placeholder="Password" v-model="password">
+        <div class="error" v-if="error">{{ error }}</div>
+        <button>Login</button>
+    </form>
+</template>
+
+<script>
+import useLogin from '@/composables/useLogin'
+import { ref } from 'vue'
+export default {
+    setup(){
+        const {error,login} = useLogin()
+
+        const email = ref("")
+        const password = ref("")
+
+        const handleSubmit = async () => {
+            const res = await login(email.value,password.value)
+            if(!error.value){
+                console.log("user logged in")
+            }
+        }
+        return {email,password,handleSubmit,error}
+    }
+}
+</script>
+
+<style>
+
+</style>
